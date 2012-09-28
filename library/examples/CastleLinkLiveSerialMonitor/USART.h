d98da3b19feb6b53300e8eb914b57ca9430b7302
picciux@gmail.com <picciux@gmail.com@dd624f7e-0c46-9956-71d9-c925e7dcd26f>
2012-09-28 08:46:50 +0000

    initial import

---
+++ library/examples/CastleLinkLiveSerialMonitor/USART.h
@@ -0,0 +1,37 @@
+
+/*****************************************************************************
+ *  CastleLinkLiveSerialMonitor - USART.h
+ *  Copyright (C) 2012  Matteo Piscitelli
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *  For further info, check http://code.google.com/p/castlelinklive4arduino/
+ *
+ *****************************************************************************/
+
+#ifndef USART_H
+#define USART_H
+
+void txbuf(uint8_t *b, uint16_t count);
+void txstr(char *str);
+void tx(char data);
+unsigned char rx(void);
+unsigned char rx_nb(void);
+void uart_flush_rxbuffer();
+void uart_init(uint16_t baudrate);
+void uart_enable_interrupt();
+void uart_disable_interrupt();
+
+
+#endif
---
+++ library/examples/CastleLinkLiveSerialMonitor/protocol.h
@@ -0,0 +1,64 @@
+
+/*****************************************************************************
+ *  CastleLinkLiveSerialMonitor - protocol.h
+ *  Copyright (C) 2012  Matteo Piscitelli
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *  For further info, check http://code.google.com/p/castlelinklive4arduino/
+ *
+ *****************************************************************************/
+
+
+/************** PROTOCOL DEFINES ******************/
+#define OUT_DATA_HEADER_H                  0xFF
+#define OUT_DATA_HEADER_L                  0xF0
+
+#define ESC_ID_MASK                        0x07 // 0000 0111
+#define THROTTLE_PRESENT                   0x08 // 0000 1000
+
+#define OUT_RESPONSE_HEADER_H              0x55
+#define OUT_RESPONSE_HEADER_L              0xAA
+
+#define RESPONSE_ACK                       0x01
+
+#define CMD_HEADER                         0x00
+#define CMD_NOOP                           0x00
+#define CMD_HELLO			   0x01
+#define CMD_SET_TMIN		           0x02
+#define CMD_SET_TMAX		           0x03
+#define CMD_SET_TMODE		           0x04
+#define CMD_SET_NESC		           0x05
+#define CMD_START		           0x06
+#define CMD_ARM				   0x07
+#define CMD_SET_THROTTLE                   0x08
+#define CMD_DISARM			   0x09
+
+#define BUFSIZE  3
+
+#define STATUS_HELLO                          0
+#define STATUS_CONF                           1
+#define STATUS_STARTED                        2
+#define STATUS_ARMED                          3
+
+#define OUT_DATA_BUFSIZE                      25
+
+typedef struct cmd_struct {
+  uint8_t id;
+  uint8_t l;
+  uint8_t h;
+  struct cmd_struct *next;
+} COMMAND;
+
+
