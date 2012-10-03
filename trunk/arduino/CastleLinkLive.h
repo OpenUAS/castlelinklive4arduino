402bbd07f8321750cac9252b25b45f21f5ab49ca
picciux@gmail.com <picciux@gmail.com@dd624f7e-0c46-9956-71d9-c925e7dcd26f>
2012-10-03 12:15:11 +0000

    Moved DISABLE_ALL_PULLUPS define and related comments to CastleLinkLive_config.h

--- trunk/arduino/CastleLinkLive.h
+++ trunk/arduino/CastleLinkLive.h
@@ -70,21 +70,6 @@
 #define MAX_ESCS 2
 #endif
 
-/*
- * Since castle pins are externally pulled-up as required by castle link live spec,
- * we need to keep disabled internal pullups when pins are used as inputs. We have two choices:
- * 1) globally disable all port pullups
- * 2) do a two-step process on every switch: 
-        - from OUTPUT to INPUT: since pins are HIGH when switching them to input mode, immediately
-                                write them LOW to disable the pullups
-        - from INPUT to OUTPUT: switching the pin directly to output would cause an output LOW
-                                towards the ESC(s), which we don't want; so write them HIGH before, temporarily
-                                enabling pullups, then switch them to output, obtaining them to stay HIGH.
-                                
- * You can choose option 1 by keeping following define un-commented, otherwise option 2 will be enforced
- */
-#define DISABLE_ALL_PULLUPS
-
 #define FRAME_RESET            -1
 #define FRAME_REFERENCE        0
 #define FRAME_VOLTAGE          1
