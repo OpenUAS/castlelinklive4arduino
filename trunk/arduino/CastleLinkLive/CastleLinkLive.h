c0292aa08eb61919205109d58c4217d307ff90e7
picciux@gmail.com <picciux@gmail.com@dd624f7e-0c46-9956-71d9-c925e7dcd26f>
2012-10-12 19:08:53 +0000

    Minor changes in documentation.

--- trunk/arduino/CastleLinkLive/CastleLinkLive.h
+++ trunk/arduino/CastleLinkLive/CastleLinkLive.h
@@ -75,10 +75,10 @@
     
     \section License
      This library is free software: you can redistribute it and/or modify
-     it under the terms of the 
-     GNU General Public License as published by
+     it under the terms of the GNU General Public License as published by
      the Free Software Foundation, either version 3 of the License, or
-     (at your option) any later version.
+     (at your option) any later version. See full license details at 
+     http://www.gnu.org/licenses/gpl.html
     
 */
 
